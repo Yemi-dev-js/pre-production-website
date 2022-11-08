41e125eec22758e24bac856671f51b97bbf7428e
yemdev-A <19147177+Yemi-dev-js@users.noreply.github.com>
2022-11-08 15:17:18 +0000

    completed most of form page, starting footer

--- vendors/js/funtionality.js
+++ vendors/js/funtionality.js
@@ -37,6 +37,40 @@
 //       prevEl: ".swiper-button-prev",
 //     },
 //   });
+////////////////////////////////////////
+
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("navbar_11");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+} 
+
+// document.getElementById("color_button").onclick = function(){execute()}
+
+// var navbar_11 = document.getElementById("navbar_11");
+// var menu = document.getElementById("menu");
+
+// window.onscroll= function(){
+//     if(window.scrollY >= menu.offsetTop){
+//         navbar_11.classList.add("sticky");
+//     }else {
+//         navbar_11.classList.remove("sticky");
+//     }
+// };
 
 
   ///////////////////////////////
